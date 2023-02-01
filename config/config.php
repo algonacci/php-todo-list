a5e2d4c44544991effe269adce5b8a7142bbdd0d
Eric Julianto <algonacci1@gmail.com>
2023-02-01 18:59:40 +0700

    Initial setup

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("ROOT_URL", "http://localhost/php-todo-list/");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "todo-list-php");
--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-echo "Hello World!";
+require "connection.php";
+
+?>
+
+<?php include "include/header.php"; ?>
+
+<div class="container">
+    <h1>To Do List</h1>
+        <div id="newtask">
+            <input type="text" placeholder="Add Tasks">
+            <button id="push">Add</button>
+        </div>
+    <div id="tasks">
+
+    </div>
+</div>
+</body>
+</html>
